9f17ed9c476314d9c65f747782de95611d89afe4
FM_Suhail <demonbruster@gmail.com>
2021-09-13 06:31:15 +0530

    remove links

--- src/templates/Hero.tsx
+++ src/templates/Hero.tsx
@@ -12,7 +12,7 @@ const Hero = () => (
     <Section yPadding="py-6">
       <NavbarTwoColumns logo={<Logo xl />}>
         <li>
-          <Link href="https://github.com/ixartz/Next-JS-Landing-Page-Starter-Template">
+          <Link href="">
             <a>GitHub</a>
           </Link>
         </li>
@@ -34,7 +34,7 @@ const Hero = () => (
         }
         description="The easiest way to build a React landing page in seconds."
         button={
-          <Link href="https://creativedesignsguru.com/category/nextjs/">
+          <Link href="">
             <a>
               <Button xl>Download Your Free Theme</Button>
             </a>
